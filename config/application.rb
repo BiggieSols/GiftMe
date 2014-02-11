50c92ad5739d7fa1704ffba5364ae22b0d32dfeb
Sol <solomon.garger@gmail.com>
2014-02-11 13:21:52 -0800

    update config file to fix heroku error

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module GiftMe
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # app does not initialize during precompile. prevents a heroku error
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], scope: "email, publish_stream"
+  provider :facebook, ENV['FACEBOOK_APP_ID'], 
+                      ENV['FACEBOOK_SECRET'], 
+                      scope: "email, user_about_me, user_birthday, user_interests, publish_stream, friends_birthday, friends_interests, friends_about_me"
 end
