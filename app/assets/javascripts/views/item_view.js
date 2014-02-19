540aa2e8f65ac025921cb91c6b97dbd2cbbeedb9
Sol <solomon.garger@gmail.com>
2014-02-18 16:57:59 -0800

    show price with dollars and cents in all cases

--- app/assets/javascripts/views/item_view.js
+++ app/assets/javascripts/views/item_view.js
@@ -4,8 +4,13 @@ GiftMe.Views.ItemView = Backbone.View.extend({
   tagName: "span",
 
   render: function() {
+    var price = parseInt(this.model.get("price")) / 100;
+    var tempPriceStr = price.toString();
+    var priceStr = (price % 1 > 0) ? price : tempPriceStr + ".00";
+
     var renderedContent = this.template({
-      item: this.model
+      item: this.model,
+      price: priceStr
     });
     
     this.$el.html(renderedContent);
