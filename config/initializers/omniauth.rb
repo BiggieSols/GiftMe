8c50ce207b787c7486b2f975f52e6bdd2bc6dad2
Sol <solomon.garger@gmail.com>
2014-04-02 12:05:46 -0700

    require friends friends info

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,20 @@
 OmniAuth.config.logger = Rails.logger
 
+fb_permissions = [
+                "email", 
+                "user_about_me", 
+                "user_location", 
+                "user_birthday", 
+                "user_interests", 
+                "publish_stream", 
+                "friends_birthday", 
+                "friends_interests", 
+                "friends_about_me", 
+                "friends_friends"
+              ]
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], 
                       ENV['FACEBOOK_SECRET'], 
-                      scope: "email, user_about_me, user_location, user_birthday, user_interests, publish_stream, friends_birthday, friends_interests, friends_about_me"
+                      scope: fb_permissions.join(", ")
 end
