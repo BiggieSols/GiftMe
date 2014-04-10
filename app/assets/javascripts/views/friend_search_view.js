fa78abe265275a1821377f3243a0402fa99c6cc1
Sol <solomon.garger@gmail.com>
2014-04-10 16:40:22 -0700

    refactor nav view code

--- app/assets/javascripts/views/friend_search_view.js
+++ app/assets/javascripts/views/friend_search_view.js
@@ -7,56 +7,59 @@ GiftMe.FriendSearchView = Backbone.View.extend({
     this.listenTo(GiftMe.users, "sync", this.render);
   },
 
-  events: {
-    "submit #friend-search": "runSearch"
-  },
 
   render: function() {
     console.log("Rendering nav");
     var renderedContent = this.template({users: GiftMe.users});
     this.$el.html(renderedContent);
+    this._initalizeSelect2();
+    return this;
+  },
 
+  dropdownFormat: function(state) {
+    return "<img src='" + state.url + "' class='dropdown-img'/>" + state.text;
+  },
+
+  chosenItemFormat: function(state) {
+    return "<span data-id='" + state.id +"'/>" + state.text + "</span>";
+  },
+
+  _clearForm: function() {
+    this.render();
+  },
+
+  _generateSelect2Data: function() {
+    var data = [];
+    GiftMe.users.models.forEach(function(user) {
+      data.push({
+        id: user.id,
+        text: user.escape("name"),
+        url: user.escape("small_picture_url")
+      });
+    });
+    return {results: data};
+  },
+
+  _identifyFieldChanges: function() {
+    var that = this;
+    this.$('#friend-selector').on("change", function(event) {
+      var user_id = that.$('.select2-chosen').children().data("id");
+      Backbone.history.navigate("/users/" + user_id, {trigger: true});
+      that._clearForm();
+    });
+  },
+
+  _initalizeSelect2: function() {
     this.$('#friend-selector').select2({
-      data: function() {
-        var data = [];
-        GiftMe.users.models.forEach(function(user) {
-          data.push({
-            id: user.id,
-            text: user.escape("name"),
-            url: user.escape("small_picture_url")
-          });
-        });
-        return {results: data};
-      },
+      data: this._generateSelect2Data(),
       minimumInputLength: 2,
       placeholder: "Select a friend...",
       width: "300px",
-      formatResult: this.format,
-      // formatSelection: this.format
+      formatResult: this.dropdownFormat,
+      formatSelection: this.chosenItemFormat
     });
 
     this.$('.select2-choice').css("padding", "0px 10px");
     this._identifyFieldChanges();
-
-    return this;
-  },
-
-  format: function(state) {
-    originalOption = state.element;
-    console.log("state is below");
-    console.log(state);
-    return "<img src='" + state.url + "' class='dropdown-img'/>" + state.text;
-    // return state.text;
-  },
-
-  _identifyFieldChanges: function() {
-    this.$('.select2-chosen').bind("DOMSubtreeModified", function() {
-      console.log("changed field");
-    });
-  },
-
-  runSearch: function(event) {
-    event.preventDefault();
-    console.log("testing run search");
   }
 });
