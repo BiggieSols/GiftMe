f2878a2765db78ca82e5caecacfa333e1a664b86
Sol <solomon.garger@gmail.com>
2014-04-23 17:19:52 -0700

    show poopver with lightbox to search for a friend

--- app/assets/javascripts/views/onboard_view.js
+++ app/assets/javascripts/views/onboard_view.js
@@ -6,7 +6,8 @@ GiftMe.Views.OnboardView = Backbone.View.extend({
   },
 
   events: {
-    "click #yourself":"loadOwnPage",
+    "click #yourself":"_loadOwnPage",
+    "click #friends":"_navPopover"
   },
 
   render: function() {
@@ -15,7 +16,30 @@ GiftMe.Views.OnboardView = Backbone.View.extend({
     return this;
   },
 
-  loadOwnPage: function() {
+  _loadOwnPage: function() {
     Backbone.history.navigate("items", {trigger: true});
+  },
+
+  _navPopover: function() {
+    var friendSearchJQObj = $('#friend-search');
+
+    friendSearchJQObj
+      .attr({
+        "data-toggle": "body",
+        "data-placement": "bottom",
+        "data-content": "search for your facebook friends to see their wishlist and recommend",
+        "data-title": "search for friends"
+      })
+      .popover('show');
+
+    $('#lightbox').css("display", "inline-block");
+
+    $('.select2-input')
+      .on("focus", function(event){
+        friendSearchJQObj.popover('hide');
+        $('#lightbox').css("display", "none");
+      }
+    );
   }
+
 });
